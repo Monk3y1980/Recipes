ad449c33b75242e89c8db008d457e616e6fa5fdc
Evgeny <eugen.eisner@gmail.com>
2021-09-02 19:41:17 +0200

    add Cors Headers

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,3 +126,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
